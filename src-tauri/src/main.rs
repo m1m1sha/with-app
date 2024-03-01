508ff7dc38196bdc48339c4e045649729054faf7
m1m1sha <18262227804@163.com>
2024-03-01 16:08:57 +0800

    ✨ feat: with 基本信息实体

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod with;
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
