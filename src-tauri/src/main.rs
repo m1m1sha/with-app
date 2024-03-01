72571a339a6c1023c2aceafaecd154572a57e5d4
m1m1sha <18262227804@163.com>
2024-03-01 21:46:46 +0800

    ✨ feat: 增加日志输出

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,11 @@ mod utils;
 mod with;
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![])
+        // .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
