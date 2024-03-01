dd9e27bef4f66a464d2d5e6b654b29c424c345c1
m1m1sha <18262227804@163.com>
2024-03-01 19:38:57 +0800

    ✨ feat: 获取运行目录

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,3 +1,25 @@
 mod root;
 
+use encoding::{all::GBK, DecoderTrap, Encoding};
+
 pub use root::is_app_elevated;
+
+pub fn current_dir() -> String {
+    let mut path = std::env::current_dir()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+
+    if path.contains('�') {
+        // 使用gbk
+        if let Ok(s) = GBK.decode(path.as_bytes(), DecoderTrap::Strict) {
+            path = s;
+        }
+    }
+
+    path
+}
+
+pub fn current_bin_dir() -> String {
+    format!("{}\\bin", current_dir())
+}
