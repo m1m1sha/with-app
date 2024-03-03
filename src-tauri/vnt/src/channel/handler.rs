24bc99bf44cdb8a43bcf355595cce2d1c25fae47
m1m1sha <18262227804@163.com>
2024-03-03 11:15:40 +0800

    🐳 chore: 移除 vnt

--- src-tauri/vnt/src/channel/handler.rs
+++
@@ -1,6 +0,0 @@
-use crate::channel::context::Context;
-use crate::channel::RouteKey;
-
-pub trait RecvChannelHandler: Clone + Send + 'static {
-    fn handle(&mut self, buf: &mut [u8], route_key: RouteKey, context: &Context);
-}
