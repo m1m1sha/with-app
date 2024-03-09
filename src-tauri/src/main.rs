266dc1ac5c86b0bee83f9d53d5d02e550cb59c17
m1m1sha <18262227804@163.com>
2024-03-09 16:41:21 +0800

    🔧 build: fs依赖

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     tracing_subscriber::fmt::init();
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         // .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
