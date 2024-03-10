5ed88fa8c728413a117d375b6fc1162fcc7871c3
m1m1sha <18262227804@163.com>
2024-03-10 18:35:07 +0800

    ✨ feat: 管理员权限打包

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
+use tauri_build::{try_build, Attributes, WindowsAttributes};
+
 fn main() {
-    tauri_build::build()
+    try_build(
+        Attributes::new().windows_attributes(
+            WindowsAttributes::new().app_manifest(include_str!("manifest.xml")),
+        ),
+    )
+    .expect("failed to run build script");
 }
