44669d770ae2e715f32c79a30a25cc857d2b7686
m1m1sha <18262227804@163.com>
2024-03-10 20:15:33 +0800

    🔧 build: 删除无用依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import Layouts from 'vite-plugin-vue-layouts'
-import Markdown from 'unplugin-vue-markdown/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
-import UnoCSS from 'unocss/vite'
-import VueDevTools from 'vite-plugin-vue-devtools'
-import LinkAttributes from 'markdown-it-link-attributes'
-import Shiki from 'markdown-it-shikiji'
-import VueRouter from 'unplugin-vue-router/vite'
-import { VueRouterAutoImports } from 'unplugin-vue-router'
-import { TDesignResolver } from 'unplugin-vue-components/resolvers'
+import path from "node:path";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import Layouts from "vite-plugin-vue-layouts";
+import VueMacros from "unplugin-vue-macros/vite";
+import UnoCSS from "unocss/vite";
+import VueDevTools from "vite-plugin-vue-devtools";
+import VueRouter from "unplugin-vue-router/vite";
+import { VueRouterAutoImports } from "unplugin-vue-router";
+import { TDesignResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+      "~/": `${path.resolve(__dirname, "src")}/`,
     },
   },
   plugins: [
@@ -33,75 +30,50 @@ export default defineConfig(async () => ({
 
     // https://github.com/posva/unplugin-vue-router
     VueRouter({
-      routesFolder: 'src/pages',
-      extensions: ['.vue', '.md'],
-      dts: 'src/typed-router.d.ts',
+      routesFolder: "src/pages",
+      extensions: [".vue", ".md"],
+      dts: "src/typed-router.d.ts",
     }),
 
     Components({
       resolvers: [
         TDesignResolver({
-          library: 'vue-next',
+          library: "vue-next",
         }),
       ],
       // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
       // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/components.d.ts",
     }),
 
     AutoImport({
       // targets to transform
       include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/],
       imports: [
-        'vue',
-        'pinia',
+        "vue",
+        "pinia",
         VueRouterAutoImports,
         {
           // add any other imports you were relying on
-          'vue-router/auto': ['useLink'],
+          "vue-router/auto": ["useLink"],
         },
       ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/components', 'src/composables', 'src/contants', 'src/stores'],
+      dts: "src/auto-imports.d.ts",
+      dirs: ["src/components", "src/composables", "src/contants", "src/stores"],
       vueTemplate: true,
       resolvers: [
         TDesignResolver({
-          library: 'vue-next',
+          library: "vue-next",
         }),
       ],
-    }),
-
-    // https://github.com/unplugin/unplugin-vue-markdown
-    // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
-    Markdown({
-      wrapperClasses: 'prose prose-sm m-auto text-left',
-      headEnabled: true,
-      async markdownItSetup(md) {
-        md.use(LinkAttributes, {
-          matcher: (link: string) => /^https?:\/\//.test(link),
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
-        })
-        md.use(
-          await Shiki({
-            defaultColor: false,
-            themes: {
-              light: 'vitesse-light',
-              dark: 'vitesse-dark',
-            },
-          }),
-        )
-      },
     }),
 
     UnoCSS(),
 
     Pages({
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
     }),
 
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
@@ -120,7 +92,7 @@ export default defineConfig(async () => ({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ['**/src-tauri/**'],
+      ignored: ["**/src-tauri/**"],
     },
   },
-}))
+}));
