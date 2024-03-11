cf6938eaea50320199a3c8ac348f03f0a0404513
m1m1sha <18262227804@163.com>
2024-03-11 11:19:42 +0800

    ✨ feat: 托盘

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,49 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{sync::Mutex, vec};
+use std::sync::Mutex;
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 mod core;
 mod util;
 
 fn main() {
     tracing_subscriber::fmt::init();
+    let tray_menu = SystemTrayMenu::new(); // insert the menu items here
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
         .manage(core::WithState(Mutex::new(None)))
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![
             core::with_start,
             core::with_stop,
             core::with_route,
             core::with_status
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
