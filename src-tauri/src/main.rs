393048d6f830a7ab1db1cc455e0adf21a93c4282
m1m1sha <18262227804@163.com>
2024-03-13 13:15:50 +0800

    🌈 style: 格式化

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use core::WithState;
 use std::sync::Mutex;
 use tauri::Manager;
 
