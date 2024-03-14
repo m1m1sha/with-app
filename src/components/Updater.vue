57a1f2b07b2b793736906ae03a3a48a251ff7722
m1m1sha <18262227804@163.com>
2024-03-14 15:45:31 +0800

    🐞 fix: 修复自动更新

--- src/components/Updater.vue
+++ src/components/Updater.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { checkUpdate } from '@tauri-apps/api/updater';
+import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
 import { MessagePlugin } from 'tdesign-vue-next';
 import pkg from "../../package.json"
+import { relaunch } from '@tauri-apps/api/process';
 
 const appStore = useAppStore();
 const { appUpdaterInfo, appUpdaterVisible, appUpdaterLoading } = storeToRefs(appStore);
@@ -12,10 +13,18 @@ async function update() {
 
     if (shouldUpdate) {
         await winIPBroadcastStop(false)
-        if (appUpdaterInfo.value) {
-            await openExternal(`https://hub.gitmirror.com/https://github.com/m1m1sha/with-app/releases/download/v${appUpdaterInfo.value!.version}/with_${appUpdaterInfo.value!.version}_x64-setup.exe`);
-        }
-        await openExternal(`https://github.com/m1m1sha/with-app/releases`);
+        MessagePlugin.error(`准备更新...`);
+        // Install the update. This will also restart the app on Windows!
+        await installUpdate()
+
+        // On macOS and Linux you will need to restart the app manually.
+        // You could use this step to display another confirmation dialog.
+        await relaunch()
+
+        // if (appUpdaterInfo.value) {
+        //     await openExternal(`https://hub.gitmirror.com/https://github.com/m1m1sha/with-app/releases/download/v${appUpdaterInfo.value!.version}/with_${appUpdaterInfo.value!.version}_x64-setup.exe`);
+        // }
+        // await openExternal(`https://github.com/m1m1sha/with-app/releases`);
     }
 }
 
@@ -36,8 +45,9 @@ onMounted(async () => {
         <div px-8>
             <t-space direction="vertical" size="small">
                 <div>当前自动更新存在bug, 更新将打开Github Release</div>
-                <div> {{ `最新版本号: ${appUpdaterInfo ? 'v' + appUpdaterInfo!.version : ''} ---- 当前(v${pkg.version})` }}
-                </div>
+                <t-link @click="openExternal(`https://github.com/m1m1sha/with-app/releases`)">{{ `最新版本号:
+                    ${appUpdaterInfo ? 'v' + appUpdaterInfo!.version : ''} ---- 当前(v${pkg.version})`
+                    }}</t-link>
                 <div>发布日期: {{ appUpdaterInfo?.date }}</div>
                 <div w-full truncate>更新简介: {{ appUpdaterInfo?.body }}...</div>
             </t-space>
