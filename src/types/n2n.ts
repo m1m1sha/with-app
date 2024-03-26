d89b01103bb67ba8fa24e63224624e61fece3f51
m1m1sha <18262227804@163.com>
2024-03-26 14:14:24 +0800

    ✨ feat: 前端n2n错误枚举

--- src/types/n2n.ts
+++ src/types/n2n.ts
@@ -1,61 +1,61 @@
 export interface EdgeArgs {
-  /// 组名
+  /** 组名 */
   c: string; //
-  /// 服务器
+  /** 服务器 */
   l: string; //
-  /// 本地端口
+  /** 本地端口 */
   p: number;
-  /// pmtu
+  /** pmtu */
   D: boolean;
-  /// 本地广播ip
+  /** 本地广播ip */
   e: string;
-  /// 中继模式
+  /** 中继模式 */
   S: EdgeRelayMode; //
-  /// 打洞间隔
+  /** 打洞间隔 */
   i: number;
-  /// 打洞ttl
+  /** 打洞ttl */
   L: number;
-  /// 组密码
+  /** 组密码 */
   k: string; //
-  /// 加密模式
+  /** 加密模式 */
   A: EdgeEncryptMode; //
-  /// 报头加密
+  /** 报头加密 */
   H: boolean;
-  /// 压缩模式
+  /** 压缩模式 */
   Z: EdgeZipMode;
-  /// 基于往返时间选择服务节点
+  /** 基于往返时间选择服务节点 */
   selectRtt: boolean;
-  /// 基于mac选择服务节点
+  /** 基于mac选择服务节点 */
   selectMac: boolean;
 
-  /// 虚拟ip
+  /** 虚拟ip */
   a: string;
-  /// mac
+  /** mac */
   m: string;
-  /// 网卡名称
+  /** 网卡名称 */
   d: string;
-  /// mtu
+  /** mtu */
   M: number;
-  /// 通过组转发数据
+  /** 通过组转发数据 */
   r: boolean;
-  /// 接受多播
+  /** 接受多播 */
   E: boolean; //
-  /// 设备描述|昵称
+  /** 设备描述|昵称 */
   I: string; //
-  /// 服务节点认证密码
+  /** 服务节点认证密码 */
   J: string; //
-  /// 节点验证公钥
+  /** 节点验证公钥 */
   P: string;
-  /// 路由规则
+  /** 路由规则 */
   R: string[];
-  /// 网卡跃点
+  /** 网卡跃点 */
   x: number; //
 
-  /// 管理端口
+  /** 管理端口 */
   t: number;
-  /// 管理密码
+  /** 管理密码 */
   managerPasswd: string;
-  /// 输出日志
+  /** 输出日志 */
   v: EdgeTraceMode;
 }
 
@@ -147,5 +147,72 @@ export enum EdgeFlag {
   SupernodeInfo = "SupernodeInfo",
   PacketStats = "PacketStats",
   EdgeInfo = "EdgeInfo",
-  Error = "Error",
+}
+
+export enum N2nError {
+  // n2n error
+  /** 授权错误 */
+  BadAuth = 0,
+  /** 方法错误 */
+  BadType,
+  /** 无法访问 */
+  NoAccess,
+  /** 命令不存在 */
+  NoCmd,
+  /** 文件不存在 */
+  NoFile,
+  /** 选项不存在 */
+  NoOptions,
+  /** 方式不存在 */
+  NoType,
+  /** 只读 */
+  ReadOnly,
+  /** 只写 */
+  WriteOnly,
+  /** 未实现 */
+  UnImplemented,
+  /** 未知命令 */
+  UnknownCmd,
+  /** 未知细目 */
+  UnknownTopic,
+
+  /** 边缘节点未启动 */
+  EdgeIsStopped,
+  /** 边缘节点启动失败 */
+  EdgeStartFailed,
+
+  /** 未知错误 */
+  Unknown,
+
+  // UDP socket error
+  /** UDP端口已被使用 */
+  SocketAddrInUse,
+  /** UDP连接超时 */
+  SocketConnectTimeout,
+  /** UDP连接失败 */
+  SocketConnectFailed,
+  /** UDP发送失败 */
+  SocketSendFailed,
+  /** UDP接收失败 */
+  SocketRecvFailed,
+  /** UDP读等待超时 */
+  SocketReadableTimeout,
+  /** UDP解析数据失败 */
+  SocketParseFailed,
+
+  // action error
+  /** action 接收失败 */
+  ActionChannelRecvFailed,
+  /** action 接收通道已关闭 */
+  ActionChannelRecvClosed,
+  /** action 发送通道已满 */
+  ActionChannelSendFull,
+  /** action 发送失败 */
+  ActionChannelSendFailed,
+  /** action 发送通道已关闭 */
+  ActionChannelSendClosed,
+
+  // arg error
+  /** arg 无效数据 */
+  ArgsInvalid,
 }
