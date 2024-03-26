4ea20e30aed1efa536b9090169b4ff3b6e9ae945
m1m1sha <18262227804@163.com>
2024-03-26 18:29:10 +0800

    🎈 perf: 优化n2n类型

--- src/composables/n2nEdge.ts
+++ src/composables/n2nEdge.ts
@@ -10,57 +10,69 @@ import {
   EdgeRespVerbose,
 } from "~/types/n2n";
 
-export async function status() {
+export async function status(): Promise<{
+  Status: EdgeRespStatus;
+}> {
   return await invoke<{
     Status: EdgeRespStatus;
-    Error: string;
   }>("edge_action", { flag: EdgeFlag.Status });
 }
 export async function stop() {
   return await invoke<{
     Stop: EdgeRespStatus;
-    Error: string;
   }>("edge_action", { flag: EdgeFlag.Stop });
 }
 
-export async function community() {
+export async function community(): Promise<{
+  Community: EdgeRespCommunity;
+}> {
   return await invoke<{
     Community: EdgeRespCommunity;
-    Error: string;
   }>("edge_action", { flag: EdgeFlag.Community });
 }
 
-export async function edges() {
+export async function edges(): Promise<{
+  EdgeInfo: EdgeRespEdges;
+}> {
   return await invoke<{
-    EdgeInfo: EdgeRespEdges | undefined;
-    Error: string | undefined;
+    EdgeInfo: EdgeRespEdges;
   }>("edge_action", { flag: EdgeFlag.EdgeInfo });
 }
 
-export async function packetStats() {
+export async function packetStats(): Promise<{
+  PacketStats: EdgeRespPacketStats;
+}> {
   return await invoke<{
-    PacketStats: EdgeRespPacketStats | undefined;
-    Error: string | undefined;
+    PacketStats: EdgeRespPacketStats;
   }>("edge_action", { flag: EdgeFlag.PacketStats });
 }
 
-export async function supernodes() {
+export async function supernodes(): Promise<{
+  SupernodeInfo: EdgeRespSupernodes;
+}> {
   return await invoke<{
-    SupernodeInfo: EdgeRespSupernodes | undefined;
-    Error: string | undefined;
+    SupernodeInfo: EdgeRespSupernodes;
   }>("edge_action", { flag: EdgeFlag.SupernodeInfo });
 }
 
-export async function timestamps() {
+export async function timestamps(): Promise<{
+  Timestamps: EdgeRespTimestamps;
+}> {
   return await invoke<{
-    Timestamps: EdgeRespTimestamps | undefined;
-    Error: string | undefined;
+    Timestamps: EdgeRespTimestamps;
   }>("edge_action", { flag: EdgeFlag.Timestamps });
 }
 
-export async function verbose() {
+export async function verbose(): Promise<{
+  Verbose: EdgeRespVerbose;
+}> {
   return await invoke<{
-    Verbose: EdgeRespVerbose | undefined;
-    Error: string | undefined;
+    Verbose: EdgeRespVerbose;
   }>("edge_action", { flag: EdgeFlag.Verbose });
+}
+
+export async function start(): Promise<null> {
+  return await invoke<null>("edge_start", {
+    args: { c: "110110", l: "101.34.37.43:7654", t: 5644 },
+  });
 }
